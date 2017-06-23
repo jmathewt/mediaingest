65279dde644df113f6442f2cb82ba3501645b65f
Jobin Thomas <jobinthomas@nypl.org>
2017-06-23 14:47:51 -0400

    Return 404 when a bag is not found. Return 200 when a bag was found successfully

--- src/main/java/org/nypl/mediaingest/controller/RequestAcceptController.java
+++ src/main/java/org/nypl/mediaingest/controller/RequestAcceptController.java
@@ -1,10 +1,13 @@
 package org.nypl.mediaingest.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.nypl.mediaingest.models.Asset;
 import org.nypl.mediaingest.service.RequestProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,9 +21,14 @@ public class RequestAcceptController {
   private RequestProcessor requestProcessor;
   
   @RequestMapping(value = "/bag/{bagName}", method = RequestMethod.GET)
-  public List<Asset> listMediaFiles(@PathVariable String bagName){ 
+  public ResponseEntity<List<Asset>> listMediaFiles(@PathVariable String bagName){ 
     System.out.println("Going to retrieve assets for bag - " + bagName);
-    return requestProcessor.getVideoFilesOfBag(bagName);
+    Optional<List<Asset>> optionalAssets = requestProcessor.getVideoFilesOfBag(bagName);
+    if(!optionalAssets.isPresent()){
+      return new ResponseEntity<List<Asset>>(HttpStatus.NOT_FOUND);
+    }else {
+      return new ResponseEntity<List<Asset>>(optionalAssets.get(), HttpStatus.OK);
+    }
   }
 
 }
--- src/main/java/org/nypl/mediaingest/service/RequestProcessor.java
+++ src/main/java/org/nypl/mediaingest/service/RequestProcessor.java
@@ -1,6 +1,7 @@
 package org.nypl.mediaingest.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.nypl.mediaingest.models.Asset;
 import org.nypl.mediaingest.models.AssetDAO;
@@ -18,12 +19,17 @@ public class RequestProcessor {
   @Autowired
   private AssetDAO assetDAO;
   
-  public List<Asset> getVideoFilesOfBag(String bagName){
-    Source source = sourceDAO.findByName(bagName);
-    int sourceId = source.getId();
-    System.out.println(sourceId);
-    List<Asset> assets = assetDAO.findBySourceId(sourceId);
-    return assets;
+  public Optional<List<Asset>> getVideoFilesOfBag(String bagName){
+    Optional<Source> optionalSource = Optional.ofNullable(sourceDAO.findByName(bagName));
+    if(!optionalSource.isPresent())
+      return Optional.empty();
+    else{
+      Source source = optionalSource.get();
+      int sourceId = source.getId();
+      System.out.println(sourceId);
+      List<Asset> assets = assetDAO.findBySourceId(sourceId);
+      return Optional.of(assets);
+    }
   }
 
 }
